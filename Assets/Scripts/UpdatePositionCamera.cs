e300ead277524fa30fe4fcded1e39954174365a3
MiguelRodriguesSilva <rm92358@estudante.fieb.edu.br>
2023-08-05 20:47:34 -0300

    Miguel - 05/08/2023 20:47

--- Assets/Scripts/UpdatePositionCamera.cs
+++ Assets/Scripts/UpdatePositionCamera.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 public class UpdatePositionCamera : MonoBehaviour
 {
     public Transform player;
+    Transform reajuste;
     public float velocidadeCamera;
+    public Transform pontoMin, pontoMax;
+
+    private void Start()
+    {
+        transform.position = player.position; 
+    }
     private void FixedUpdate()
+    {
+        if (player.position.x >)
+        CameraSmooth();
+        
+    }
+
+    void CameraSmooth()
     {
         transform.position = Vector2.Lerp(transform.position, player.position, velocidadeCamera);
     }
