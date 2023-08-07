686c08bfc8bd968ca52fa3ada0a67296fa606c2d
Miguel Rodrigues Silva <rm92358@estudante.fieb.edu.br>
2023-08-07 17:52:57 -0300

    Versão Miguel - 07/08/2023

--- Assets/Scripts/UpdatePositionCamera.cs
+++ Assets/Scripts/UpdatePositionCamera.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class UpdatePositionCamera : MonoBehaviour
 {
+    [SerializeField]
     public Transform player;
     Transform reajuste;
     public float velocidadeCamera;
     public Transform pontoMin, pontoMax;
+    public bool estaNoLimite;
+    public string direcao;
 
     private void Start()
     {
@@ -15,13 +20,58 @@ public class UpdatePositionCamera : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (player.position.x >)
+        if ( estaNoLimite == false )
         CameraSmooth();
         
     }
 
+    private void Update()
+    {
+        if ( estaNoLimite == false)
+        {
+            if (player.position.x < pontoMin.position.x)
+            {
+                direcao = "Esquerda";
+            }
+
+            if (player.position.x > pontoMin.position.x)
+            {
+                direcao = "Direita";
+            }
+        }
+
+        if(estaNoLimite == true || player.position.x > transform.position.x || direcao == "Esquerda")
+        {
+            estaNoLimite = false;
+        }
+
+        if (estaNoLimite == true || player.position.x < transform.position.x || direcao == "Direta")
+        {
+            estaNoLimite = false;
+        }
+
+
+    }
+
+
     void CameraSmooth()
     {
         transform.position = Vector2.Lerp(transform.position, player.position, velocidadeCamera);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "BarreiraCamera")
+        {
+            estaNoLimite = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "BarreiraCamera")
+        {
+
+        }
     }
 }
